3799f62fcafd440ae25c2f09b895f880800bca04
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-08-30 12:00:08 +0200

    def: Update blobs list
    Change-Id: I5c0548edfa1778afc59e77153e04a5b9a9a62d77

--- extract-files.sh
+++ extract-files.sh
@@ -25,6 +25,3 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 ANDROID_ROOT="$MY_DIR"/../../..
 BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-CAMERA_HAL="$BLOB_ROOT"/vendor/lib64/hw/com.qti.chi.override.so
-sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "$CAMERA_HAL"
